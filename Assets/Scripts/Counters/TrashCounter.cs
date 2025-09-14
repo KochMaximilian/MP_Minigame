9a935d9525735e349f6aae5cb737b9754cece3d4
Maximilian Koch <maximiliankochhome@gmail.com>
2025-09-14 15:02:42 +0200

    implemented trashcounter

--- Assets/Scripts/Counters/TrashCounter.cs
+++ Assets/Scripts/Counters/TrashCounter.cs
@@ -1,16 +1,10 @@
 using UnityEngine;
 
-public class TrashCounter : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+public class TrashCounter : BaseCounter {
+    public override void Interact(Player player) {
+        if (player.HasKitchenObject()) {
+            player.GetKitchenObject().DestroySelf();
+        }
     }
 }
+
