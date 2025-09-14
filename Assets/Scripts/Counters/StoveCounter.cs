f016f35ba2296868ff68ae3f6fde58ac3135191a
Maximilian Koch <maximiliankochhome@gmail.com>
2025-09-14 17:06:50 +0200

    setup state machine for frying counter

--- Assets/Scripts/Counters/StoveCounter.cs
+++ Assets/Scripts/Counters/StoveCounter.cs
@@ -1,12 +1,57 @@
 using UnityEngine;
 using static CuttingCounter;
 
-public class StoveCounter : BaseCounter
-{
+public class StoveCounter : BaseCounter {
+    private enum State {
+        Idle,
+        Frying,
+        Fried,
+        Burned,
+    }
+    private State currentState;
     [SerializeField] private FryingRecipeSO[] fryingRecipeSOArray;
+    [SerializeField] bool isDebugging = false;
+    private float fryingTimer;
+    private FryingRecipeSO fryingRecipeSO;
+
+    private void Start() {
+        currentState = State.Idle;
+    }
+
+    private void Update() {
+
+        if (HasKitchenObject()) {
+            switch (currentState) {
+                case State.Idle:
+                    break;
+                case State.Frying:
+                    fryingTimer += Time.deltaTime;
+                    if (fryingTimer > fryingRecipeSO.fryingTimerMax) {
+                        //Fried
+                        GetKitchenObject().DestroySelf();
+                        KitchenObject.SpawnKitchenObject(fryingRecipeSO.output, this);
+                        if (isDebugging) {
+                            Debug.Log(fryingRecipeSO.input + " Fried");
+                        }
+                      
+                        currentState = State.Fried;
+                    
+                    }
+                    break;
+                case State.Fried:
+                    break;
+                case State.Burned:
+                    break;
+            }
+            if (isDebugging) {
+                Debug.Log("Frying Time: " + (int)fryingTimer);
+                Debug.Log("Stove State: " + currentState);
+            }
+        }
+    }
 
     public override void Interact(Player player) {
- 
+
         if (!HasKitchenObject()) {
             // There is no KitchenObject
             if (player.HasKitchenObject()) {
@@ -14,7 +59,10 @@ public class StoveCounter : BaseCounter
                 if (HasRecipeWithInput(player.GetKitchenObject().GetKitchenObjectSO())) {
                     // player carrying something that can be fried
                     player.GetKitchenObject().setKitchenObjectParent(this); // Drop KitchenObject on empty Counter
-                   // TODO Progress 
+                    fryingRecipeSO = GetFryingRecipeSOWithInput(GetKitchenObject().GetKitchenObjectSO());
+                    currentState = State.Frying;
+                    fryingTimer = 0f;
+                    // TODO Progress 
                 }
             } else {
                 // Player not carrying anything
@@ -27,7 +75,7 @@ public class StoveCounter : BaseCounter
             } else {
                 // Player is not carrying anything 
                 GetKitchenObject().setKitchenObjectParent(player);
-               // TODO Progress
+                // TODO Progress
             }
         }
     }
