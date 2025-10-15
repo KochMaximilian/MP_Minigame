b51492923a98e04f3b47e5cd5af7cd2c99e27cce
Maximilian Koch <maximiliankochhome@gmail.com>
2025-10-15 21:44:36 +0200

    set up order display

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -1,8 +1,13 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DeliveryManager : MonoBehaviour {
+
+    public event EventHandler OnRecipeSapwn;
+    public event EventHandler OnRecipeCompleted;
     public static DeliveryManager Instance { get; private set; }
+
 
     [SerializeField] private RecipeListSO recipeListSO;
     [SerializeField] private bool enableDeliveryDebugLogs = false;
@@ -22,9 +27,12 @@ public class DeliveryManager : MonoBehaviour {
         if (spawnRecipeTimer < 0f) {
             spawnRecipeTimer = spawnRecipeTimerMax;
             if (waitingRecipeSOList.Count < waitingRecipesMax) {
-                RecipeSO waitingRecipeSO = recipeListSO.recipeSOList[Random.Range(0, recipeListSO.recipeSOList.Count)];
-                Debug.Log(waitingRecipeSO.recipeName);
+                RecipeSO waitingRecipeSO = recipeListSO.recipeSOList[UnityEngine.Random.Range(0, recipeListSO.recipeSOList.Count)];
+              //  Debug.Log(waitingRecipeSO.recipeName);
                 waitingRecipeSOList.Add(waitingRecipeSO);
+
+
+                OnRecipeSapwn?.Invoke(this, EventArgs.Empty);
             }
         }
     }
@@ -70,6 +78,7 @@ public class DeliveryManager : MonoBehaviour {
                         Debug.Log("Player delivered the correct recipe");
                     }
                     waitingRecipeSOList.RemoveAt(i);
+                    OnRecipeCompleted?.Invoke(this, EventArgs.Empty);
                     return;
                 }
             }
@@ -78,5 +87,9 @@ public class DeliveryManager : MonoBehaviour {
         if (enableDeliveryDebugLogs) {
             Debug.Log("Player did not deliver the correct recipe");
         }
+    }
+
+    public List<RecipeSO> GetWaitingRecipeSOList() {
+        return waitingRecipeSOList;
     }
 }
---
+++ Assets/Scripts/UI/OrderUI.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class OrderUI : MonoBehaviour
+{
+    [SerializeField] private Transform container;
+    [SerializeField] private Transform recipeTemplate;
+
+    private void Awake() {
+        recipeTemplate.gameObject.SetActive(false);
+    }
+
+    private void Start() {
+        DeliveryManager.Instance.OnRecipeSapwn += DeliveryManager_OnRecipeSapwn;
+        DeliveryManager.Instance.OnRecipeCompleted += DeliveryManager_OnRecipeCompleted;
+
+        UpdateVisual();
+    }
+
+    private void DeliveryManager_OnRecipeCompleted(object sender, System.EventArgs e) {
+        UpdateVisual();
+    }
+
+    private void DeliveryManager_OnRecipeSapwn(object sender, System.EventArgs e) {
+        UpdateVisual();
+    }
+
+    private void UpdateVisual() {
+        foreach (Transform child in container) {
+            if (child == recipeTemplate) continue;
+            Destroy(child.gameObject);
+            
+        }
+
+         foreach (RecipeSO recipeSO in DeliveryManager.Instance.GetWaitingRecipeSOList()) {
+            Transform recipeTransfrom =  Instantiate(recipeTemplate, container);
+            recipeTransfrom.gameObject.SetActive(true);
+
+        }
+    }
+}
+
